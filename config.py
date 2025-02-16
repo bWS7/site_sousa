1595bbe778de7e7105be85961edb57b415abebd0
bWS7 <brunowsantos15@gmail.com>
2025-02-15 22:01:54 -0300

    att banco

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'sua_chave_secreta_aqui')
     
     # Configuração do banco de dados
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:ars291576@localhost:5432/test')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://bruno_master:FYdi6BeYH3jRL0YOqt4XmInNwUOJlr0S@dpg-cul7u3jv2p9s73a4ru2g-a.oregon-postgres.render.com/test_4jyn')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desabilita o rastreamento de modificações para economizar recursos
     
     # Configuração da pasta de uploads
