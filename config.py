2702d739216ffc3624b10788438148fb77c4195a
bWS7 <brunowsantos15@gmail.com>
2025-04-02 15:08:50 -0300

    test

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'sua_chave_secreta_aqui')
     
     # Configuração do banco de dados
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://bruno_master:FYdi6BeYH3jRL0YOqt4XmInNwUOJlr0S@dpg-cul7u3jv2p9s73a4ru2g-a.oregon-postgres.render.com/test_4jyn')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://bruno_master:hvVWwBYQDjRM03w5PRjgcC1tVz723aBA@dpg-cvmn2fodl3ps73dgod00-a.oregon-postgres.render.com/bd_sousa_26xp')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desabilita o rastreamento de modificações para economizar recursos
     
     # Configuração da pasta de uploads
